c2a2ed8501c0ea4d124d0b1e1942c8da0025d520
Leping08 <dereks008@gail.com>
2023-07-03 14:47:13 -0400

    Add nav titles

--- app/Library/Nav.php
+++ app/Library/Nav.php
@@ -13,18 +13,22 @@ class Nav
         return [
             [
                 'text' => 'Home',
+                'title' => 'Land Planning Associates Home Page',
                 'link' => route('home'),
             ],
             [
                 'text' => 'Services',
+                'title' => 'Land Planning Associates Services',
                 'link' => route('services'),
             ],
             [
                 'text' => 'About',
+                'title' => 'Land Planning Associates About Us',
                 'link' => route('about'),
             ],
             [
                 'text' => 'Contact Us',
+                'title' => 'Land Planning Associates Contact Us',
                 'link' => route('contact'),
             ],
         ];
