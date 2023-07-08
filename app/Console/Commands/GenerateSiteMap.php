4d5829f3a6f4c9b0e2b4f98dbbc7e822c237369d
Leping08 <dereks008@gail.com>
2023-07-07 22:07:50 -0400

    Add site map

---
+++ app/Console/Commands/GenerateSiteMap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        // modify this to your own needs
+        SitemapGenerator::create('https://lpabob.com/')
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
